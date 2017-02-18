8ab0a7a9457a6b2158aef4fd07a4a3e57299bb21
Jason Kridner <jdk@ti.com>
2017-02-18 05:11:36 +0000

    Add rc_get_state

--- rc-node-bindings.cc
+++ rc-node-bindings.cc
@@ -13,9 +13,30 @@ namespace rc {
     static void RCexit(void*) {
         rc_cleanup();
     }
+
+    void RCgetState(const v8::FunctionCallbackInfo<v8::Value>& args) {
+        v8::Isolate* isolate = args.GetIsolate();
+        rc_state_t s = rc_get_state();
+        switch(s) {
+	    case RUNNING:
+            args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "RUNNING"));
+            break;
+	    case PAUSED:
+            args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "PAUSED"));
+            break;
+	    case EXITING:
+            args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "EXITING"));
+            break;
+        case UNINITIALIZED:
+        default:
+            args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "UNINITIALIZED"));
+            break;
+        }
+    }
     
     void module_init(v8::Local<v8::Object> exports) {
         NODE_SET_METHOD(exports, "initialize", RCinitialize);
+        NODE_SET_METHOD(exports, "get_state", RCgetState);
         node::AtExit(RCexit);
     }
 
--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var assert = require('assert');
 
 assert.equal(rc.initialize(), false);
-
 fs.readFile('/var/run/robotics_cape.pid', readRCpid);
+assert.equal(rc.get_state(), "UNINITIALIZED");
 
 function readRCpid(err, data) {
     assert.equal(data, process.pid);
