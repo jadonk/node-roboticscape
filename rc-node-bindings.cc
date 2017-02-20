c49f2efea72a47f773f6eda6309843ea67f925c5
Jason Kridner <jdk@ti.com>
2017-02-20 21:38:16 +0000

    Feeling pretty lost

--- rc-node-bindings.cc
+++ rc-node-bindings.cc
@@ -10,20 +10,45 @@ extern "C" {
 
 typedef void (*void_fp)();
 
-template <typename T>
-struct Callback;
+/*
+template<typename TCallable>
+struct Wrapper
+{
+    typedef Wrapper<TCallable> Self;
 
-template <typename Ret, typename... Params>
-struct Callback<Ret(Params...)> {
-    template <typename... Args>
-    static Ret callback(Args... args) { return func(args...); }
-    static std::function<Ret(Params...)> func;
+    TCallable function;
+
+    Wrapper(TCallable const & function)
+    : function(function)
+    {
+        // Nothing else
+    }
+
+    static void call()
+    {
+        Self * wrapper = reinterpret_cast<Self * >();
+        wrapper->function();
+    }
 };
 
-// Initialize the static member.
-template <typename Ret, typename... Params>
-std::function<Ret(Params...)> Callback<Ret(Params...)>::func;
+typedef Wrapper<std::function<void()>> MyWrapper;
+*/
 
+// OT => Object Type
+// RT => Return Type
+// A ... => Arguments
+template<typename OT, typename RT, typename ... A>
+struct lambda_expression {
+    OT _object;
+    RT(OT::*_function)(A...)const;
+
+    lambda_expression(const OT & object)
+        : _object(object), _function(&decltype(_object)::operator()) {}
+
+    RT operator() (A ... args) const {
+        return (_object.*_function)(args...);
+    }
+};
 
 namespace rc {
     void RCinitialize(const Nan::FunctionCallbackInfo<v8::Value>& info) {
@@ -77,25 +102,61 @@ namespace rc {
     }
     
     struct Handoff {
+        Handoff() {
+        };
+        Handoff(const Handoff& other) {
+            std::swap(async, (uv_async_t &)other.async);
+            cb.SetFunction(other.cb.GetFunction());
+        };
+        Handoff& operator=(Handoff other)
+        {
+            std::swap(async, other.async);
+            cb.SetFunction(other.cb.GetFunction());
+            return *this;
+        }
+        Handoff& operator=(const Handoff &other)
+        {
+            std::swap(async, (uv_async_t &)other.async);
+            cb.SetFunction(other.cb.GetFunction());
+            return *this;
+        }
         uv_async_t async;
         Nan::Callback cb;
         void handler() {
             uv_async_send(&async);
         }
-        template<typename T>
-        void_fp getHandler(void (T::*method)(), T* r) {
-            Callback<void()>::func = std::bind(method, r);
+
+        /*
+        auto lambdaHandler() {
+            auto lambda = [=]() {
+                handler();
+            };
+            return lambda_expression<decltype(lambda), void>(lambda);
+        }
+        void_fp getHandler() {
+            return (void_fp) std::mem_fn(&Handoff::handler);
+        }
+        void_fp getHandler() {
+            std::function<void()>::func callback = std::bind(method, this);
             void (*c_function_pointer)() = 
                 static_cast<decltype(c_function_pointer)>(Callback<void()>::callback);
+            fprintf(stderr, "getHandler: %p, %p, %p\n",
+                (void *)method, (void *)this, (void *)c_function_pointer);
+            fflush(stderr);
             return c_function_pointer;
         }
+        */
     };
     
     static void doHandoff(uv_async_t* handle) {
         Nan::HandleScope scope;
+        fprintf(stderr, "doHandoff: %p\n", (void *)handle);
+        fflush(stderr);
         Handoff *h = static_cast<Handoff *>(handle->data);
         h->cb.Call(0, 0);
     }
+
+    Handoff handoffPausePressed;
 
     void RCsetPausePressed(const Nan::FunctionCallbackInfo<v8::Value>& info) {
         if (info.Length() != 1) {
@@ -107,15 +168,21 @@ namespace rc {
             return;
         }
         v8::Local<v8::Function> fn = info[0].As<v8::Function>();
-        Handoff *h = new Handoff();
-        h->async.data = h;
-        h->cb.SetFunction(fn);
+        handoffPausePressed.async.data = &handoffPausePressed;
+        handoffPausePressed.cb.SetFunction(fn);
         uv_loop_t *loop = uv_default_loop();
-        uv_async_init(loop, &(h->async), doHandoff);
-        void_fp fp = h->getHandler(&Handoff::handler, h);
-        rc_set_pause_pressed_func(fp);
+        uv_async_init(loop, &(handoffPausePressed.async), doHandoff);
+        //auto fp = [&] { h->handler(); };
+        std::function<void()> f_handler = std::bind(&Handoff::handler,
+            handoffPausePressed);
+        void_fp * fp = f_handler.target<void_fp>();
+        fprintf(stderr, "RCsetPausePressed: %p, %p\n", 
+            (void *)&handoffPausePressed, (void *)fp);
+        fflush(stderr);
+        rc_set_pause_pressed_func(*fp);
     }
     
+    /*
     void RCsetPauseReleased(const Nan::FunctionCallbackInfo<v8::Value>& info) {
         if (info.Length() != 1) {
             Nan::ThrowTypeError("Wrong number of arguments (should be 1)");
@@ -126,13 +193,16 @@ namespace rc {
             return;
         }
         v8::Local<v8::Function> fn = info[0].As<v8::Function>();
-        Handoff *h = new Handoff();
+        Handoff *h = new Handoff(fn);
+        fprintf(stderr, "RCsetPauseReleased: %p\n", (void *)h);
+        fflush(stderr);
         h->async.data = h;
         h->cb.SetFunction(fn);
         uv_loop_t *loop = uv_default_loop();
         uv_async_init(loop, &(h->async), doHandoff);
-        void_fp fp = h->getHandler(&Handoff::handler, h);
-        rc_set_pause_released_func(fp);
+        std::function<void()> f_handler = std::bind(&Handoff::handler, h);
+        void_fp * fp = f_handler.target<void_fp>();
+        rc_set_pause_released_func(*fp);
     }
     
     void RCsetModePressed(const Nan::FunctionCallbackInfo<v8::Value>& info) {
@@ -145,13 +215,16 @@ namespace rc {
             return;
         }
         v8::Local<v8::Function> fn = info[0].As<v8::Function>();
-        Handoff *h = new Handoff();
+        Handoff *h = new Handoff(fn);
+        fprintf(stderr, "RCsetModePressed: %p\n", (void *)h);
+        fflush(stderr);
         h->async.data = h;
         h->cb.SetFunction(fn);
         uv_loop_t *loop = uv_default_loop();
         uv_async_init(loop, &(h->async), doHandoff);
-        void_fp fp = h->getHandler(&Handoff::handler, h);
-        rc_set_mode_pressed_func(fp);
+        std::function<void()> f_handler = std::bind(&Handoff::handler, h);
+        void_fp * fp = f_handler.target<void_fp>();
+        rc_set_mode_pressed_func(*fp);
     }
     
     void RCsetModeReleased(const Nan::FunctionCallbackInfo<v8::Value>& info) {
@@ -164,15 +237,19 @@ namespace rc {
             return;
         }
         v8::Local<v8::Function> fn = info[0].As<v8::Function>();
-        Handoff *h = new Handoff();
+        Handoff *h = new Handoff(fn);
+        fprintf(stderr, "RCsetModeReleased: %p\n", (void *)h);
+        fflush(stderr);
         h->async.data = h;
         h->cb.SetFunction(fn);
         uv_loop_t *loop = uv_default_loop();
         uv_async_init(loop, &(h->async), doHandoff);
-        void_fp fp = h->getHandler(&Handoff::handler, h);
-        rc_set_mode_released_func(fp);
+        std::function<void()> f_handler = std::bind(&Handoff::handler, h);
+        void_fp * fp = f_handler.target<void_fp>();
+        rc_set_mode_released_func(*fp);
     }
-    
+    */
+
     void ModuleInit(v8::Local<v8::Object> exports) {
         exports->Set(Nan::New("initialize").ToLocalChecked(),
             Nan::New<v8::FunctionTemplate>(RCinitialize)->GetFunction());
@@ -182,12 +259,14 @@ namespace rc {
             Nan::New<v8::FunctionTemplate>(RCsetState)->GetFunction());
         exports->Set(Nan::New("set_pause_pressed_func").ToLocalChecked(),
             Nan::New<v8::FunctionTemplate>(RCsetPausePressed)->GetFunction());
+            /*
         exports->Set(Nan::New("set_pause_released_func").ToLocalChecked(),
             Nan::New<v8::FunctionTemplate>(RCsetPauseReleased)->GetFunction());
         exports->Set(Nan::New("set_mode_pressed_func").ToLocalChecked(),
             Nan::New<v8::FunctionTemplate>(RCsetModePressed)->GetFunction());
         exports->Set(Nan::New("set_mode_released_func").ToLocalChecked(),
             Nan::New<v8::FunctionTemplate>(RCsetModeReleased)->GetFunction());
+            */
         node::AtExit(RCexit);
     }
 
